ba613d628ea9681804caab409b92e1e81bc01414
oppsig <oppsig@users.noreply.github.com>
2016-01-20 22:31:00 +0100

    Added some aliases.

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,2 +1,4 @@
 alias reload!='. ~/.zshrc'
 alias cfg-etc='vim /etc/default'
+alias getc='nzbget -o outputmode=curses -C'
+alias apt5='sudo apt-get check && sudo apt-get update && sudo apt-get dist-upgrade && sudo apt-get autoremove --purge && sudo apt-get autoclean'
