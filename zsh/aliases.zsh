28e73ddd81fd39bc530cf02356e66a7f17907a52
oppsig <oppsig@noreply.users.github.com>
2016-09-02 19:40:47 +0200

    Edit vimrc and zsh aliases.

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -4,6 +4,8 @@ alias getc='nzbget -o outputmode=curses -C'
 alias apt5='sudo apt-get check && sudo apt-get update && sudo apt-get dist-upgrade && sudo apt-get autoremove --purge && sudo apt-get autoclean'
 
 alias cls='clear' # Good 'ol Clear Screen command
+alias e-aliases='vim ~/.dotfiles/zsh/aliases.zsh'
+alias e-mopidy='vim ~/.config/mopidy/mopidy.conf'
 ssh() {
 	if [ "$(ps -p $(ps -p $$ -o ppid=) -o comm=)" = "tmux" ]; then
 		tmux rename-window "$*"
