c5a93fdc95413c499105c890099e2119e2e27d8a
oppsig <oppsig@users.noreply.github.com>
2018-07-24 11:01:30 +0200

    Add undo for neovim. Add /home/$USER/.local/bin for user to $PATH.

--- system/_path.zsh
+++ system/_path.zsh
@@ -1,2 +1,2 @@
-export PATH="/usr/local/bin:/usr/local/sbin:$DOTFILES/bin:$PATH"
+export PATH="/home/$USER/.local/bin:/usr/local/bin:/usr/local/sbin:$DOTFILES/bin:$PATH"
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
