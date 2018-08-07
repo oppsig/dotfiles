71a8373a543f6f8d9eb2077287ec6fe442565fbc
oppsig <oppsig@users.noreply.github.com>
2018-08-07 12:11:27 +0200

    Path changes and vim edits.

--- system/_path.zsh
+++ system/_path.zsh
@@ -1,2 +1,2 @@
-export PATH="/home/$USER/.local/bin:/usr/local/bin:/usr/local/sbin:$DOTFILES/bin:$PATH"
+export PATH="/home/$USER/.local/bin:/home/kodi/.gem/ruby/2.5.0/bin:/usr/local/bin:/usr/local/sbin:$DOTFILES/bin:$PATH"
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
